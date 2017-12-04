4590e5183d3ecf4e0cd99828ee2e65ec884c7aae
Ubuntu <ubuntu@ip-172-31-42-233.ec2.internal>
2017-12-04 01:52:48 +0000

    Style updates

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <Link to="/">Home</Link>
 
     <nav>
-      <Link to="/helloworld">Hello World</Link>
+      <Link to="/restaurants">All Restaurants</Link>
     </nav>
 
     <hr />
